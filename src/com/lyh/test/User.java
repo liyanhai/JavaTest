f127af306dfc7b0e177cde447b4456ee2e641b5a
lyh_glb <lyh_glb@atguigu.com>
2019-01-12 11:10:13 +0800

    初次提交JavaTest项目

---
+++ src/com/lyh/test/User.java
@@ -0,0 +1,28 @@
+package com.lyh.test;
+
+/**
+ * @author liyanhai
+ * @create 2019-01-12 11:08
+ */
+public class User {
+
+    private String name;
+    private Integer id;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
